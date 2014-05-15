8e37eb96af86f40bb3d5f9248f302e99444dd1a5
Guts <julien.moura@gmail.com>
2014-05-15 23:12:42 +0200

    clean up and calling submodule

--- jobs_georezo.py
+++ jobs_georezo.py
@@ -1,46 +1,29 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/env python
 
-#import os
+from os import path
 import feedparser
 import sqlite3
 
 
-def extract(caracter):
-    ''' Fonction qui extrait l'intitulé et le lieu en fonction
-    d'un caractère de séparation '''
-    t = entry.title.split(']')[1].split(caracter)
-    # Si caracter est présent plus d'une fois
-    if len(t) > 2:
-        j = ''
-        for e in t[:-1]:
-            j = j + ' ' + e
-        t = j.strip()
-    else:
-        t = t[0]
-    l = entry.title.split(']')[1].split(caracter)[-1].strip()
-    return t, l
+# Custom modules
+from modules import analyseur
 
-# Si la BD n'existe pas, je la crée
-# if 'elpaso.sqlite' not in os.listdir('.'):
 
-#     conn = sqlite3.connect('bd_jobs_georezo.db')
-#     c = conn.cursor()
-#     c.execute('''CREATE TABLE jobs
-#                  (id int, category text, title text,
-#                   date timestamp, lieu text)''')
-# # Sinon, je me connecte à la BD
-# else:
-conn = sqlite3.connect('/home/pvernier/code/python/elpaso/elpaso.sqlite')
+# connexion à BD
+conn = sqlite3.connect(path.abspath('elpaso.sqlite'))
 c = conn.cursor()
 
 # Ce fichier contient l'id de la dernière annonce traitée
-fichier = open('/home/pvernier/code/python/elpaso/last_id_georezo.txt', 'r')
+fichier = open(path.abspath('last_id_georezo.txt'), 'r')
 last_id = int(fichier.readline())
 fichier.close()
 
 # l'URL du flux RSS des annonces
 d = feedparser.parse('http://georezo.net/extern.php?fid=10')
+
+# liste des identifiants des nouvelles offres
+li_id = []
 
 compteur = 0
 for entry in d.entries:
@@ -48,7 +31,7 @@ for entry in d.entries:
     # La première annonce traitée est la dernière publiée, donc celle
     # qui a l'id le plus grand. Je mets cet id dans le fichier texte.
     if d.entries.index(entry) == 0:
-        fichier = open('/home/pvernier/code/python/elpaso/last_id_georezo.txt', 'w')
+        fichier = open(path.abspath('last_id_georezo.txt'), 'w')
         fichier.write(str(job_id))
         fichier.close()
     # Si l'id de l'annonce est supérieur à l'id du fichier, cela signifie
@@ -60,6 +43,7 @@ for entry in d.entries:
             # Save (commit) the changes
             conn.commit()
             compteur += 1
+            li_id.append(job_id)
         except:
             print(str(job_id))
 
@@ -69,4 +53,4 @@ print(str(compteur) + ' annonces on été ajoutées')
 
 if compteur > 0:
     print('lancer process')
-    from modules import contrats
+    analyseur.Analizer(li_id)
