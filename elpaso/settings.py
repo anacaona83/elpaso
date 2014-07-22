9d6d4bc426d8d27dd812c6da1b3c104fb8319b87
pvernier <pvernier82@gmail.com>
2014-07-22 23:51:57 +0200

    Moved to production server

--- elpaso/settings.py
+++ elpaso/settings.py
@@ -83,10 +83,11 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# STATIC_ROOT n'est utile que pour la prod
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static_elpaso"),
     os.path.join(BASE_DIR, "../../js_libs"),
 )
-
