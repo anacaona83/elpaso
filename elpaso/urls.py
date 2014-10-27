b72e4fce5b6e9455d0ba39608e0e63eb72aced6d
pvernier <pvernier82@gmail.com>
2014-10-27 18:21:57 +0100

    Added about page and restructured links

--- elpaso/urls.py
+++ elpaso/urls.py
@@ -4,8 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="jobs/base.html")),
+    url(r'^contrats/$', TemplateView.as_view(template_name="jobs/contrats_exploit_json2.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="jobs/about.html")),
+
     url(r'^contrats_json/$', 'jobs.views.contrat_json'),
-    url(r'^contrats_exploit/$', TemplateView.as_view(template_name="jobs/contrats_exploit_json2.html")),
     url(r'^contrats_camemberts/$', TemplateView.as_view(template_name="jobs/contrats_camemberts.html")),
 
     url(r'^admin/', include(admin.site.urls)),
