2cf968c5a78517a121c66f8d4f8538e69fa8b53e
pvernier <pvernier82@gmail.com>
2014-11-03 18:30:58 +0100

    Started to work on the contact form

--- jobs/views.py
+++ jobs/views.py
@@ -14,20 +14,20 @@ def stats_home(request):
     nb_months = Month.objects.count()
     nb_weeks = Week.objects.count()
 
-    if request.method == 'POST': # If the form has been submitted...
-        form = ContactForm(request.POST) # A form bound to the POST data
-        if form.is_valid():
-            sender = form.cleaned_data['sender']
-            subject = 'Contact - ' + form.cleaned_data['subject']
-            message = form.cleaned_data['message']
+    # if request.method == 'POST': # If the form has been submitted...
+    #     form = ContactForm(request.POST) # A form bound to the POST data
+    #     if form.is_valid():
+    #         sender = form.cleaned_data['sender']
+    #         subject = 'Contact - ' + form.cleaned_data['subject']
+    #         message = form.cleaned_data['message']
 
-            recipients = ['admin@datamadre.com']
+    #         recipients = ['admin@datamadre.com']
 
             
-            send_mail(subject, message, sender, recipients)
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = ContactForm() # An unbound form
+    #         send_mail(subject, message, sender, recipients)
+    #         return HttpResponseRedirect('/thanks/') # Redirect after POST
+    # else:
+    #     form = ContactForm() # An unbound form
 
 
     return render_to_response('jobs/home.html', {
@@ -36,7 +36,7 @@ def stats_home(request):
         'nb_months': nb_months,
         'nb_weeks': nb_weeks,
         'first_date': first_date,
-        'form': form,
+        #'form': form,
     })
 
 
