413ce71db0ede7d3622a33f527e5d04cbd9bb595
Guts <julien.moura@gmail.com>
2015-06-18 16:40:31 +0200

    clean up in template views

--- elpaso/urls.py
+++ elpaso/urls.py
@@ -4,16 +4,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^$', TemplateView.as_view(template_name="jobs/home.html")),
+    # url(r'^$', TemplateView.as_view(template_name="jobs/home.html")),
     url(r'^$', 'jobs.views.stats_home'),
     url(r'^contrats/$', TemplateView.as_view(template_name="jobs/contrats.html")),
+    url(r'^map/$', TemplateView.as_view(template_name="jobs/map.html")),
     url(r'^technos/$', TemplateView.as_view(template_name="jobs/technologies.html")),
     url(r'^semantique/$', TemplateView.as_view(template_name="jobs/semantique.html")),
-    url(r'^contrats_json/$', 'jobs.views.contrat_json'),
-    url(r'^contrats_camemberts/$', TemplateView.as_view(template_name="jobs/contrats_camemberts.html")),
-    url(r'^stacks/$', TemplateView.as_view(template_name="jobs/contrats_stacks_nvd3.html")),
-    url(r'^timeline/$', TemplateView.as_view(template_name="jobs/timeline.html")),
-    url(r'^contrats_bis/$', TemplateView.as_view(template_name="jobs/contrats_exploit_json2.html")),
+    url(r'^timeline/$', TemplateView.as_view(template_name="jobs/timeline/index.html")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
